acd8e9a6068643245c6ee7d3894493d943050e9e
Dan <dan.mok999@yandex.ru>
2020-10-05 20:04:51 +0500

    Tasks 3-4

--- index.js
+++ index.js
@@ -1,64 +1,58 @@
-addListeners();
+configureAnimations();
 
-function addListeners() {
-    document.getElementById('fadeInPlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('fadeInBlock');
-            animaster().fadeIn(block, 5000);
-        });
+function configureAnimations() {
+    const animator = animaster();
+    bind('fadeInPlay', () =>
+        animator.fadeIn(getTarget('fadeInBlock'), 5000));
+    bind('fadeOutPlay', () =>
+        animator.fadeOut(getTarget('fadeOutBlock'), 5000));
 
-    document.getElementById('movePlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('moveBlock');
-            animaster().move(block, 1000, {x: 100, y: 10});
-        });
+    bind('movePlay', () =>
+        animator.move(getTarget('moveBlock'), 1000, {x: 100, y: 10}));
 
-    document.getElementById('scalePlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('scaleBlock');
-            animaster().scale(block, 1000, 1.25);
-        });
+    bind('scalePlay', () =>
+        animator.scale(getTarget('scaleBlock'), 1000, 1.25));
+    bind('moveAndHidePlay', () =>
+        animator.moveAndHidePlay(
+            getTarget('moveAndHideBlock'), 
+            3000, 
+            {x: 100, y: 20}));
 }
 
-function animaster(){
+function animaster() {
     return {
         fadeIn(element, duration) {
-            element.style.transitionDuration =  `${duration}ms`;
+            element.style.transitionDuration = `${duration}ms`;
             element.classList.remove('hide');
             element.classList.add('show');
+        },
+        fadeOut(element, duration) {
+            element.style.transitionDuration = `${duration}ms`;
+            element.classList.remove('show');
+            element.classList.add('hide');
         },
         move(element, duration, translation) {
             element.style.transitionDuration = `${duration}ms`;
             element.style.transform = getTransform(translation, null);
         },
         scale(element, duration, ratio) {
-            element.style.transitionDuration =  `${duration}ms`;
+            element.style.transitionDuration = `${duration}ms`;
             element.style.transform = getTransform(null, ratio);
-        }
+        },
+        moveAndHidePlay(element, duration, translation) {
+            this.move(element, duration * (2/5), translation);
+            this.fadeOut(element, duration * (3/5));
+        },
     };
 }
 
-/**
- * Блок плавно появляется из прозрачного.
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- */
-//fadeIn
-/**
- * Функция, передвигающая элемент
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- * @param translation — объект с полями x и y, обозначающими смещение блока
- */
-// function move(element, duration, translation) {
+function getTarget(elementId) {
+    return document.getElementById(elementId);
+}
 
-/**
- * Функция, увеличивающая/уменьшающая элемент
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
- */
-//scale
+function bind(elementId, callback) {
+    document.getElementById(elementId).onclick = callback;
+}
 
 function getTransform(translation, ratio) {
     const result = [];
