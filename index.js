5e840c6550443b79dc3258cd4796b40ca902d408
Yury Frolov <yura200253@gmail.com>
2021-03-22 13:29:22 +0500

    5

--- index.js
+++ index.js
@@ -1,59 +1,66 @@
 function animaster() {
-    /**
-     * Блок плавно появляется из прозрачного.
-     * @param element — HTMLElement, который надо анимировать
-     * @param duration — Продолжительность анимации в миллисекундах
-     */
     function fadeIn(element, duration) {
         element.style.transitionDuration =  `${duration}ms`;
         element.classList.remove('hide');
         element.classList.add('show');
     }
 
-    /**
-     * Блок плавно появляется из прозрачного.
-     * @param element — HTMLElement, который надо анимировать
-     * @param duration — Продолжительность анимации в миллисекундах
-     */
     function fadeOut(element, duration) {
         element.style.transitionDuration = `${duration}ms`;
         element.classList.remove('show');
         element.classList.add('hide');
     }
 
-    /**
-     * Функция, передвигающая элемент
-     * @param element — HTMLElement, который надо анимировать
-     * @param duration — Продолжительность анимации в миллисекундах
-     * @param translation — объект с полями x и y, обозначающими смещение блока
-     */
     function move(element, duration, translation) {
         element.style.transitionDuration = `${duration}ms`;
         element.style.transform = getTransform(translation, null);
     }
 
-    /**
-     * Функция, увеличивающая/уменьшающая элемент
-     * @param element — HTMLElement, который надо анимировать
-     * @param duration — Продолжительность анимации в миллисекундах
-     * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
-     */
     function scale(element, duration, ratio) {
         element.style.transitionDuration =  `${duration}ms`;
         element.style.transform = getTransform(null, ratio);
+    }
+
+    function moveAndHide(element, duration) {
+        const moveDuration = 0.4 * duration;
+        move(element, moveDuration, {x: 100, y: 20});
+        setTimeout(() => fadeOut(element, duration - moveDuration), moveDuration);
+    }
+
+    function showAndHide(element, duration) {
+        const phaseDuration = duration / 3;
+        fadeIn(element, phaseDuration);
+        setTimeout(() => fadeOut(element, phaseDuration),
+            2 * phaseDuration);
+    }
+
+    function heartBeating(element) {
+        const timerId = setInterval(() => {
+            setTimeout(() => scale(element, 500, 1.4), 0);
+            setTimeout(() => scale(element, 500, 1/1.4), 500);
+        }, 1000);
+        return {
+            stop: function () {
+                clearInterval(timerId);
+            }
+        }
     }
 
     return {
         fadeIn: fadeIn,
         fadeOut: fadeOut,
         move: move,
-        scale: scale
+        scale: scale,
+        moveAndHide: moveAndHide,
+        showAndHide: showAndHide,
+        heartBeating: heartBeating
     }
 }
 
 addListeners();
 
 function addListeners() {
+    let heartBeat = undefined;
     const aniMaster = animaster();
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
@@ -79,10 +86,26 @@ function addListeners() {
             aniMaster.fadeOut(block, 5000);
         });
 
-    document.getElementById('fadeOutPlay')
+    document.getElementById('moveAndHidePlay')
         .addEventListener('click', function () {
-            const block = document.getElementById('fadeOutBlock');
-            aniMaster.fadeOut(block, 5000);
+            const block = document.getElementById('moveAndHideBlock');
+            aniMaster.moveAndHide(block, 5000);
+        });
+
+    document.getElementById('showAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('showAndHideBlock');
+            aniMaster.showAndHide(block, 5000);
+        });
+
+    document.getElementById('heartBeatingPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('heartBeatingBlock');
+            heartBeat = aniMaster.heartBeating(block, 5000);
+        });
+    document.getElementById('heartBeatingStop')
+        .addEventListener('click', function () {
+            heartBeat.stop();
         });
 }
 
