825787c91a6324e85a857bc001a3ee6b4ad18fb6
Vladislav <vladatkishkin@gmail.com>
2021-03-22 01:44:01 -0700

    123

--- index.js
+++ index.js
@@ -5,24 +5,21 @@ function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            ani.fadeIn(block, 5000);
-        });
-
-    document.getElementById('fadeOutPlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('fadeInBlock');
+            animaster().fadeIn(block, 5000);
             ani.fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
+            animaster().move(block, 1000, {x: 100, y: 10});
             ani.move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
+            animaster().scale(block, 1000, 1.25);
             ani.scale(block, 1000, 1.25);
         });
 }
