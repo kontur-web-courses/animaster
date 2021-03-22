b3430510517b6b13658f5779ab362d9d71b5ba13
krutovsky-danya <krutovsky.danya@gmail.com>
2021-03-22 13:21:59 +0500

    Small fix

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 addListeners();
 
 function addListeners() {
+    let ani = animaster();
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            animaster().fadeIn(block, 5000);
+            ani.fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            animaster().move(block, 1000, {x: 100, y: 10});
+            ani.move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            animaster().scale(block, 1000, 1.25);
+            ani.scale(block, 1000, 1.25);
         });
 }
 
