bfc590cc73c3800cfd434179c9b1f96a8e3b2a0e
Алёночка Савельева <savelyeva.alena@ya.ru>
2022-03-14 13:45:50 +0500

    merge artem

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 addListeners();
 
+const anim = animaster();
+
 function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            anim.fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
+            anim.move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            anim.scale(block, 1000, 1.25);
         });
 
     document.getElementById('fadeOutPlay')
@@ -27,24 +29,20 @@ function addListeners() {
 }
 
 function animaster() {
-    /**
-     * Блок плавно появляется из прозрачного.
-     * @param element — HTMLElement, который надо анимировать
-     * @param duration — Продолжительность анимации в миллисекундах
-     */
-    function moveAndHide() {
+
+    function MoveAndHide() {
         this.move(element, 400, 100);
         this.scale(element, 600, 0);
     }
 
-    function showAndHide() {
+    function ShowAndHide() {
         this.fadeIn(element, 500);
         setTimeout(500);
         this.fadeOut(element, 500);
 
     }
 
-    function heartBeating() {
+    function HeartBeating() {
         while (true){
             this.scale(element, 500, 1.4);
             this.scale(element, 500, (1/1.4));
@@ -52,6 +50,11 @@ function animaster() {
 
     }
 
+    /**
+     * Блок плавно появляется из прозрачного.
+     * @param element — HTMLElement, который надо анимировать
+     * @param duration — Продолжительность анимации в миллисекундах
+     */
     function FadeIn(element, duration) {
         element.style.transitionDuration =  `${duration}ms`;
         element.classList.remove('hide');
@@ -80,7 +83,7 @@ function animaster() {
  * @param duration — Продолжительность анимации в миллисекундах
  * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
  */
-function scale(element, duration, ratio) {
+    function Scale(element, duration, ratio) {
     element.style.transitionDuration =  `${duration}ms`;
     element.style.transform = getTransform(null, ratio);
 }
@@ -90,7 +93,9 @@ function scale(element, duration, ratio) {
         scale: function(element, duration, ratio) {return Scale(element, duration, ratio)},
         fadeIn: function (element, duration) {return FadeIn(element, duration)},
         fadeOut: function (element, duration) {return FadeOut(element, duration)},
-
+        moveAndHide: function (){return MoveAndHide()},
+        showAndHide: function (){return ShowAndHide()},
+        heartBeating: function (){return HeartBeating()}
     }
 }
 
