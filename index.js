280ebc238ba68238a94679db2ef01b54b89e41b2
k1nque <artkashaev@gmail.com>
2022-03-14 13:37:42 +0500

    MERGE WITH TASK 4

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 addListeners();
 
-let anim = animaster();
-
 function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            anim.fadeIn(block, 5000);
+            fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            anim.move(block, 1000, {x: 100, y: 10});
+            move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            anim.scale(block, 1000, 1.25);
+            scale(block, 1000, 1.25);
         });
 }
 
@@ -28,6 +26,26 @@ function animaster() {
      * @param element — HTMLElement, который надо анимировать
      * @param duration — Продолжительность анимации в миллисекундах
      */
+    function moveAndHide() {
+        this.move(element, 400, 100);
+        this.scale(element, 600, 0);
+    }
+
+    function showAndHide() {
+        this.fadeIn(element, 500);
+        setTimeout(500);
+        this.fadeOut(element, 500);
+
+    }
+
+    function heartBeating() {
+        while (true){
+            this.scale(element, 500, 1.4);
+            this.scale(element, 500, (1/1.4));
+        }
+
+    }
+
     function FadeIn(element, duration) {
         element.style.transitionDuration =  `${duration}ms`;
         element.classList.remove('hide');
@@ -50,16 +68,16 @@ function animaster() {
         element.style.transform = getTransform(translation, null);
     }
 
-    /**
-     * Функция, увеличивающая/уменьшающая элемент
-     * @param element — HTMLElement, который надо анимировать
-     * @param duration — Продолжительность анимации в миллисекундах
-     * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
-     */
-    function Scale(element, duration, ratio) {
-        element.style.transitionDuration =  `${duration}ms`;
-        element.style.transform = getTransform(null, ratio);
-    }
+/**
+ * Функция, увеличивающая/уменьшающая элемент
+ * @param element — HTMLElement, который надо анимировать
+ * @param duration — Продолжительность анимации в миллисекундах
+ * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
+ */
+function scale(element, duration, ratio) {
+    element.style.transitionDuration =  `${duration}ms`;
+    element.style.transform = getTransform(null, ratio);
+}
 
     return {
         move: function(element, duration, translation) {return Move(element, duration, translation)},
