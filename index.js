1922aac3ce5860eac6f8977e1245a625f479f377
k1nque <artkashaev@gmail.com>
2022-03-14 13:56:52 +0500

    5 task

--- index.js
+++ index.js
@@ -30,22 +30,28 @@ function addListeners() {
 
 function animaster() {
 
-    function MoveAndHide() {
-        this.move(element, 400, 100);
-        this.scale(element, 600, 0);
+    function MoveAndHide(element) {
+        Move(element, 400, 100);
+        Scale(element, 600, 0);
     }
 
-    function ShowAndHide() {
-        this.fadeIn(element, 500);
+    function ShowAndHide(element) {
+        FadeIn(element, 500);
         setTimeout(500);
-        this.fadeOut(element, 500);
+        FadeOut(element, 500);
 
     }
 
-    function HeartBeating() {
-        while (true){
-            this.scale(element, 500, 1.4);
-            this.scale(element, 500, (1/1.4));
+    function HeartBeating(element) {
+        let inter = setInterval(function () {
+            Scale(element, 500, 1.4);
+            Scale(element, 500, (1 / 1.4));
+        }, 1000);
+        return {
+            stop: function () {
+                clearInterval(inter);
+            }
+
         }
 
     }
@@ -56,16 +62,17 @@ function animaster() {
      * @param duration — Продолжительность анимации в миллисекундах
      */
     function FadeIn(element, duration) {
-        element.style.transitionDuration =  `${duration}ms`;
+        element.style.transitionDuration = `${duration}ms`;
         element.classList.remove('hide');
         element.classList.add('show');
     }
 
-    function FadeOut(element, duration){
-        element.style.transitionDuration =  `${duration}ms`;
+    function FadeOut(element, duration) {
+        element.style.transitionDuration = `${duration}ms`;
         element.classList.remove('show');
         element.classList.add('hide');
     }
+
     /**
      * Функция, передвигающая элемент
      * @param element — HTMLElement, который надо анимировать
@@ -77,28 +84,41 @@ function animaster() {
         element.style.transform = getTransform(translation, null);
     }
 
-/**
- * Функция, увеличивающая/уменьшающая элемент
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
- */
+    /**
+     * Функция, увеличивающая/уменьшающая элемент
+     * @param element — HTMLElement, который надо анимировать
+     * @param duration — Продолжительность анимации в миллисекундах
+     * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
+     */
     function Scale(element, duration, ratio) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.style.transform = getTransform(null, ratio);
-}
+        element.style.transitionDuration = `${duration}ms`;
+        element.style.transform = getTransform(null, ratio);
+    }
 
     return {
-        move: function(element, duration, translation) {return Move(element, duration, translation)},
-        scale: function(element, duration, ratio) {return Scale(element, duration, ratio)},
-        fadeIn: function (element, duration) {return FadeIn(element, duration)},
-        fadeOut: function (element, duration) {return FadeOut(element, duration)},
-        moveAndHide: function (){return MoveAndHide()},
-        showAndHide: function (){return ShowAndHide()},
-        heartBeating: function (){return HeartBeating()}
+        move: function (element, duration, translation) {
+            return Move(element, duration, translation)
+        },
+        scale: function (element, duration, ratio) {
+            return Scale(element, duration, ratio)
+        },
+        fadeIn: function (element, duration) {
+            return FadeIn(element, duration)
+        },
+        fadeOut: function (element, duration) {
+            return FadeOut(element, duration)
+        },
+        moveAndHide: function () {
+            return MoveAndHide()
+        },
+        showAndHide: function () {
+            return ShowAndHide()
+        },
+        heartBeating: function () {
+            return HeartBeating()
+        }
     }
 }
-
 
 
 function getTransform(translation, ratio) {
