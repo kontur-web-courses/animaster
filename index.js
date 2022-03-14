53339c927357640574e85ad71f7d893b89a4ff0f
artem <2001raf@gmail.com>
2022-03-14 12:08:40 +0500

    Added moveAndHide in animaster

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function animaster(){
             element.classList.add('hide');
             element.classList.remove('show');
         },
+        moveAndHide(element, duration){
+            this.move(element, duration * 2 / 5, {x: 100, y: 20});
+            this.fadeOut(element, duration * 3 / 5);
+        },
         move(element, duration, translation) {
             element.style.transitionDuration = `${duration}ms`;
             element.style.transform = getTransform(translation, null);
@@ -41,6 +45,18 @@ function addListeners() {
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
             animaster().scale(block, 1000, 1.25);
+        });
+
+    document.getElementById('fadeOutPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('fadeOutBlock');
+            animaster().fadeOut(block, 5000);
+        });
+
+    document.getElementById('moveAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveAndHideBlock');
+            animaster().moveAndHide(block, 5000);
         });
 }
 
