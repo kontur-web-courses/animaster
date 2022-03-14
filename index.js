0f71410ea6fc91a79954ccde5a2bf27af40514c2
NitroLine <48989608+NitroLine@users.noreply.github.com>
2022-03-14 16:49:17 +0500

    add 4 task

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ function addListeners() {
             const block = document.getElementById('scaleBlock');
             animaster().scale(block, 1000, 1.25);
         });
+
+    document.getElementById('moveAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveAndHideBlock');
+            animaster().moveAndHide(block, 3000);
+        });
+
+    document.getElementById('showAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('showAndHideBlock');
+            animaster().showAndHide(block, 3000);
+        });
+
+    document.getElementById('heartBeatingPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('heartBeatingBlock');
+            animaster().heartBeating(block);
+        });
 }
 
 function animaster() {
@@ -68,6 +86,40 @@ function animaster() {
             element.style.transitionDuration = `${duration}ms`;
             element.style.transform = getTransform(null, ratio);
         },
+
+        /**
+         * Функция, передвигающая и прячущая элемент
+         * @param element — HTMLElement, который надо анимировать
+         * @param duration — Продолжительность анимации в миллисекундах
+         */
+        moveAndHide(element, duration) {
+            this.move(element, duration * 0.4, {x: 100, y: 20})
+            setTimeout(this.fadeOut, duration * 0.4, element, duration * 0.6)
+        },
+
+        /**
+         * Функция, показывающая затем прячущая элемент
+         * @param element — HTMLElement, который надо анимировать
+         * @param duration — Продолжительность анимации в миллисекундах
+         */
+        showAndHide(element, duration) {
+            this.fadeIn(element, duration / 3)
+            setTimeout(this.fadeOut, 2 * duration / 3, element, duration / 3)
+        },
+
+        /**
+         * Функция, включающая анимацию сердцебиения
+         * @param element — HTMLElement, который надо анимировать
+         */
+        heartBeating(element) {
+            let heartBeat = () => {
+                this.scale(element, 500, 1.4);
+                setTimeout(this.scale, 500, element, 500, 1)
+            }
+            heartBeat();
+            setInterval(heartBeat, 1200);
+        },
+
     }
 }
 
