12a4eb4e3c7ff21551eff78214aa9ccde1afe34d
Ilya <zdravulin@gmail.com>
2022-03-14 19:52:16 +0500

    Какая красота <3

--- index.js
+++ index.js
@@ -1,57 +1,133 @@
+function animaster() {
+    return {
+        move(element, duration, translation) {
+            let reset = (element) => {
+                element.style.transitionDuration = `0ms`;
+                element.style.transform = getTransform({x: 0, y: 0}, null);
+            }
+            element.style.transitionDuration = `${duration}ms`;
+            element.style.transform = getTransform(translation, null);
+            setTimeout(() => reset(element), duration);
+        },
+
+        fadeIn(element, duration) {
+            let reset = (element) => {
+                element.style.transitionDuration = `0ms`;
+                element.classList.remove('show');
+                element.classList.add('hide');
+            }
+
+            element.style.transitionDuration = `${duration}ms`;
+            element.classList.remove('hide');
+            element.classList.add('show');
+            setTimeout(() => reset(element), duration);
+        },
+
+        fadeOut(element, duration) {
+            let reset = (element) => {
+                element.style.transitionDuration = `0ms`;
+                element.classList.remove('hide');
+                element.classList.add('show');
+            }
+            element.style.transitionDuration = `${duration}ms`;
+            element.classList.remove('show');
+            element.classList.add('hide');
+            setTimeout(() => reset(element), duration);
+        },
+
+        scale(element, duration, ratio) {
+            let reset = (element) => {
+                element.style.transitionDuration = `0ms`;
+                element.style.transform = getTransform(null, 1);
+            }
+            element.style.transitionDuration = `${duration}ms`;
+            element.style.transform = getTransform(null, ratio);
+            setTimeout(() => reset(element), duration);
+        },
+
+        moveAndHide(element, duration) {
+            this.move(element, duration * 2 / 5, {x: 100, y: 20});
+            setTimeout(() => anime.fadeOut(element, duration), duration * 2 / 5);
+
+        },
+        showAndHide(element, duration) {
+            this.fadeIn(element, duration / 3);
+            setTimeout(() => anime.fadeOut(element, duration / 3), duration * 2 / 3);
+        },
+        hearthBeating(element) {
+            let self = this;
+            let intervalId = -1;
+            return {
+                stop() {
+                    clearInterval(intervalId);
+                },
+                start() {
+                    intervalId = setInterval(() => {
+                        self.scale(element, 500, 1.4);
+                        setTimeout(() => self.scale(element, 500, 1), 500);
+                    }, 1000)
+                }
+            }
+        }
+    }
+}
+
+let anime = animaster();
+
+
 addListeners();
+
 
 function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            anime.fadeIn(block, 5000);
+
+        });
+
+    document.getElementById('fadeOutPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('fadeOutBlock');
+            anime.fadeOut(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
+            anime.move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            anime.scale(block, 1000, 1.25);
+        });
+
+    document.getElementById('moveAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveAndHideBlock');
+            anime.moveAndHide(block, 1000);
+        });
+    document.getElementById('showAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('showAndHideBlock');
+            anime.showAndHide(block, 1000);
+        });
+
+    const block = document.getElementById('heartBeatingBlock');
+    let heartBeater = anime.hearthBeating(block);
+    document.getElementById('heartBeatingPlay')
+        .addEventListener('click', function () {
+            heartBeater.start();
+        });
+
+    document.getElementById('heartBeatingStop')
+        .addEventListener('click', function () {
+            heartBeater.stop();
         });
 }
 
-/**
- * Блок плавно появляется из прозрачного.
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- */
-function fadeIn(element, duration) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.classList.remove('hide');
-    element.classList.add('show');
-}
-
-/**
- * Функция, передвигающая элемент
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- * @param translation — объект с полями x и y, обозначающими смещение блока
- */
-function move(element, duration, translation) {
-    element.style.transitionDuration = `${duration}ms`;
-    element.style.transform = getTransform(translation, null);
-}
-
-/**
- * Функция, увеличивающая/уменьшающая элемент
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
- */
-function scale(element, duration, ratio) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.style.transform = getTransform(null, ratio);
-}
 
 function getTransform(translation, ratio) {
     const result = [];
@@ -63,3 +139,5 @@ function getTransform(translation, ratio) {
     }
     return result.join(' ');
 }
+
+
