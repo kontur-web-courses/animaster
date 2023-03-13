4c208fc47b6228ba223ea1aa5d625554a3909a2b
Konstantin <zaikinke@yandex.ru>
2023-03-13 17:50:01 +0500

    Add new listeners

--- index.js
+++ index.js
@@ -29,48 +29,18 @@ function addListeners() {
             const block = document.getElementById('moveAndHideBlock');
             animaster().moveAndHide(block, 1000, {x: 100, y: 20});
         });
+    document.getElementById('showAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('showAndHideBlock');
+            animaster().showAndHide(block, 1000);
+        });
+    document.getElementById('heartBeatingPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('heartBeatingBlock');
+            animaster().heartBeating(block, 1000, 1.25);
+        });
 }
 
-/**
- * Блок плавно появляется из прозрачного.
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- */
-function fadeIn(element, duration) {
-    animaster().fadeIn(element, duration);
-}
-
-/**
- * Функция, передвигающая элемент
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- * @param translation — объект с полями x и y, обозначающими смещение блока
- */
-function move(element, duration, translation) {
-    animaster().move(element, duration, translation);
-}
-
-/**
- * Функция, увеличивающая/уменьшающая элемент
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
- */
-function scale(element, duration, ratio) {
-    animaster().scale(element, duration, ratio);
-}
-
-function getTransform(translation, ratio) {
-    animaster().getTransform(translation, ratio);
-}
-
-function fadeOut(element, duration) {
-    animaster().fadeOut(element, duration);
-}
-
-function moveAndHide(element, duration, translation) {
-    animaster().moveAndHide(element, duration, translation);
-}
 
 function animaster(){
     return {
@@ -108,9 +78,8 @@ function animaster(){
         },
         showAndHide: function (element, duration){
             this.fadeIn(element, duration * 1/3)
-            setTimeout(function() {
-            }, duration * 1/3);
-            this.fadeOut(element, duration * 1/3)
+            setTimeout(() => this.fadeOut(element, duration * 1/3),
+                duration * 1/3);
         },
         heartBeating: function (element, duration, ratio){
             for (let i = 0; i < Infinity; i++) {
