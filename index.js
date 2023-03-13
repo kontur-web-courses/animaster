efc68382eeb045c6e55f0c6942fab188d4fe8fab
Pohotlivi Ded <wolf.alex.shishkin@gmail.com>
2023-03-13 19:19:55 +0500

    fshekfhskjehfkes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 addListeners();
 
+var stop = null;
+
 function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
@@ -36,10 +38,52 @@ function addListeners() {
             const block = document.getElementById('showAndHideBlock');
             animaster().showAndHide(block, 5000);
         });
+
+    document.getElementById('heartBeatingPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('heartBeatingBlock');
+            stop = animaster().heartBeating(block, 1000);
+        });
+
+    document.getElementById('heartBeatingStop')
+        .addEventListener('click', function () {
+            stop.stop();
+        });
+
+    document.getElementById('moveAndHideReset')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveAndHideBlock');
+            animaster().resetMoveAndHide(block);
+        });
+
+    document.getElementById('customAnimationPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('customAnimationBlock');
+            const customAnimation = animaster()
+                                        .addMove(1000, {x: 40, y: 40})
+                                        .addMove(1000, {x: 80, y: 0})
+                                        .addMove(1000, {x: 40, y: -40});
+            customAnimation.play(block);
+        });
 }
 
 
 function animaster() {
+    var resetFadeIn = function(element) {
+        element.style.transitionDuration = null;
+        element.classList.remove('hide');
+        element.classList.add('show');
+    };
+    var resetFadeOut = function(element){
+        element.style.transitionDuration = null;
+        element.classList.remove('show');
+        element.classList.add('hide');
+    };
+    var resetMoveAndScale = function(element){
+        element.style.transitionDuration = null;
+        element.style.transform = null;
+    };
+
     return {
         fadeIn: function (element, duration) {
             element.style.transitionDuration =  `${duration}ms`;
@@ -68,10 +112,44 @@ function animaster() {
             setTimeout(() => {this.fadeOut(element, duration * 0.6);}, duration * 0.4);
         },
 
+        resetMoveAndHide: function (element) {
+            resetFadeIn(element);
+            resetMoveAndScale(element);
+        },
+
         showAndHide: function (element, duration) {
             this.fadeIn(element, duration / 3);
             setTimeout(() => {this.fadeOut(element, duration / 3);}, duration * 2 / 3);
         },
+
+        heartBeating: function (element, duration) {
+            var flag = true;
+            setInterval(function (self) {
+                if (!flag)
+                    return;
+                self.scale(element, duration / 2, 1.4);
+                setTimeout(() => {self.scale(element, duration / 2, 1);}, duration / 2);
+            }, duration, this)
+            return {stop: () => {flag = false;}};
+        },
+
+        _steps: [],
+        _durations: [0],
+
+        _addStep: function(step, duration, ...args) {
+            this._durations.push(this._durations.at(-1) + duration);
+            this._steps.push((element) => setTimeout(() => step(element, duration, ...args), this._durations.at(-1)));
+            return this;
+        },
+
+        play: function(element) {
+            this._steps.forEach(function(e) { e(element); console.log('hui'); });
+        },
+
+        addMove: function(duration, translation) {
+            this._addStep(this.move, duration, translation);
+            return this;
+        }
     }
 }
 
