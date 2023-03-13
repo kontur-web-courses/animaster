ee1a099582f8cbfdc48427a916b10441b643bae1
3a4eM-MHE-HuK <gavrilov.nikita@urfu.me>
2023-03-13 18:13:00 +0500

    punkti 1-5

--- index.js
+++ index.js
@@ -1,56 +1,134 @@
 addListeners();
 
+function animaster() {
+    function resetFadeIn(element) {
+        animaster().fadeOut(element, 0);
+    }
+
+    function resetFadeOut(element) {
+        animaster().fadeIn(element, 0);
+    }
+
+    function resetMoveAndScale(element) {
+        element.style.transitionDuration = '0s';
+        element.style.transform = getTransform({x: 0, y: 0}, 1);
+    }
+
+    return {
+        fadeIn: (element, duration) => {
+            element.style.transitionDuration = `${duration}ms`;
+            element.classList.remove('hide');
+            element.classList.add('show');
+            },
+
+        move: (element, duration, translation) => {
+            element.style.transitionDuration = `${duration}ms`;
+            element.style.transform = getTransform(translation, null);
+        },
+
+        scale: (element, duration, ratio) => {
+            element.style.transitionDuration = `${duration}ms`;
+            element.style.transform = getTransform(null, ratio);
+
+        },
+
+        fadeOut: (element, duration) => {
+            element.style.transitionDuration = `${duration}ms`;
+            element.classList.remove('show');
+            element.classList.add('hide');
+        },
+
+        moveAndHide: (element, duration) => {
+            element.style.transitionDuration = `${duration * 0.4}ms`;
+            element.style.transform = getTransform({x: 100, y: 20}, null);
+            setTimeout(() => {
+                element.style.transitionDuration = `${duration * 0.6}ms`;
+                element.classList.remove('show');
+                element.classList.add('hide');
+                },duration * 0.4)
+        },
+
+        showAndHide: (element, duration) => {
+            element.style.transitionDuration = `${duration / 3}ms`;
+            element.classList.remove('hide');
+            element.classList.add('show');
+            setTimeout(() => {
+                element.style.transitionDuration = `${duration / 3}ms`;
+                element.classList.remove('show');
+                element.classList.add('hide');
+            },duration / 3 * 2)
+        },
+
+        heartBeating: (element) => {
+            let intervalId;
+            return {
+                start: () => intervalId = setInterval(() => {
+                element.style.transitionDuration = `500ms`;
+                element.style.transform = getTransform(null, 1.4);
+                setTimeout(() => {
+                    element.style.transitionDuration = `500ms`;
+                    element.style.transform = getTransform(null,  1);
+                }, 500)}, 1000),
+
+                stop: () => {
+                    clearInterval(intervalId);
+                }
+            }
+        }
+    }
+}
+
+
+
 function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            animaster().fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
+            animaster().move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            animaster().scale(block, 1000, 1.25);
         });
-}
 
-/**
- * Блок плавно появляется из прозрачного.
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- */
-function fadeIn(element, duration) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.classList.remove('hide');
-    element.classList.add('show');
-}
+    document.getElementById('fadeOutPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('fadeOutBlock');
+            animaster().fadeOut(block, 1000);
+        });
 
-/**
- * Функция, передвигающая элемент
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- * @param translation — объект с полями x и y, обозначающими смещение блока
- */
-function move(element, duration, translation) {
-    element.style.transitionDuration = `${duration}ms`;
-    element.style.transform = getTransform(translation, null);
-}
+    document.getElementById('moveAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveAndHideBlock');
+            animaster().moveAndHide(block, 1000);
+        });
 
-/**
- * Функция, увеличивающая/уменьшающая элемент
- * @param element — HTMLElement, который надо анимировать
- * @param duration — Продолжительность анимации в миллисекундах
- * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
- */
-function scale(element, duration, ratio) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.style.transform = getTransform(null, ratio);
+    document.getElementById('showAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('showAndHideBlock');
+            animaster().showAndHide(block, 1000);
+        });
+
+    const block = document.getElementById('heartBeatingBlock');
+    const heartBeating = animaster().heartBeating(block);
+
+    document.getElementById('heartBeatingPlay')
+        .addEventListener('click', function () {
+            heartBeating.start();
+        });
+
+    document.getElementById('heartBeatingStop')
+        .addEventListener('click', function () {
+            heartBeating.stop();
+        });
 }
 
 function getTransform(translation, ratio) {
