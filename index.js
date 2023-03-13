07f208a4fd26fe724f413186a00e6a63edbc1677
polinabubb <polinabubb@gmail.com>
2023-03-13 14:14:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ function addListeners() {
             animaster().fadeIn(block, 5000);
         });
 
-    document.getElementById('fadeOutPlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('fadeOutBlock');
-            fadeOut(block, 5000);
-        });
-
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
@@ -24,100 +18,52 @@ function addListeners() {
             const block = document.getElementById('scaleBlock');
             animaster().scale(block, 1000, 1.25);
         });
-
-    document.getElementById('moveAndHidePlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('moveAndHideBlock');
-            animaster().moveAndHide(block, 1000);
-        });
-
-    document.getElementById('showAndHidePlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('showAndHideBlock');
-            animaster().showAndHide(block, 1000);
-        });
-    document.getElementById('heartBreathPlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('heartBreathBlock');
-            animaster().heartBreath(block, 1000);
-        });
 }
 
-function animaster() {
+function animaster()
+{
     return {
         /**
-         * Блок плавно появляется из прозрачного.
-         * @param element — HTMLElement, который надо анимировать
-         * @param duration — Продолжительность анимации в миллисекундах
-         */
+ * Блок плавно появляется из прозрачного.
+ * @param element — HTMLElement, который надо анимировать
+ * @param duration — Продолжительность анимации в миллисекундах
+ */
         fadeIn: function (element, duration) {
-            element.style.transitionDuration = `${duration}ms`;
+            element.style.transitionDuration =  `${duration}ms`;
             element.classList.remove('hide');
             element.classList.add('show');
         },
         /**
-         * Блок плавно пропадает.
-         * @param element — HTMLElement, который надо анимировать
-         * @param duration — Продолжительность анимации в миллисекундах
-         */
-        fadeOut: function (element, duration) {
-            element.style.transitionDuration = `${duration}ms`;
-            element.classList.add('hide');
-            element.classList.remove('show');
-        },
-
-        /**
-         * Функция, передвигающая элемент
-         * @param element — HTMLElement, который надо анимировать
-         * @param duration — Продолжительность анимации в миллисекундах
-         * @param translation — объект с полями x и y, обозначающими смещение блока
-         */
+ * Функция, передвигающая элемент
+ * @param element — HTMLElement, который надо анимировать
+ * @param duration — Продолжительность анимации в миллисекундах
+ * @param translation — объект с полями x и y, обозначающими смещение блока
+ */
         move: function (element, duration, translation) {
             element.style.transitionDuration = `${duration}ms`;
             element.style.transform = getTransform(translation, null);
         },
-
         /**
-         * Функция, увеличивающая/уменьшающая элемент
-         * @param element — HTMLElement, который надо анимировать
-         * @param duration — Продолжительность анимации в миллисекундах
-         * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
-         */
+ * Функция, увеличивающая/уменьшающая элемент
+ * @param element — HTMLElement, который надо анимировать
+ * @param duration — Продолжительность анимации в миллисекундах
+ * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
+ */
         scale: function (element, duration, ratio) {
-            element.style.transitionDuration = `${duration}ms`;
+            element.style.transitionDuration =  `${duration}ms`;
             element.style.transform = getTransform(null, ratio);
-        },
-
-        moveAndHide(element, duration) {
-            animaster().move(element, duration * 2 / 5, {x: 100, y: 20})
-            animaster().fadeOut(element, duration * 3 / 5)
-        },
-
-        showAndHide(element, duration) {
-            animaster().fadeIn(element, duration / 3)
-            setTimeout(() => {
-                animaster().fadeOut(element, duration / 3)
-            }, duration / 3);
-        },
-
-        heartBreath(element, duration) {
-            animaster().scale(element, 1000, 1.4);
-            setTimeout(() => {
-                animaster().fadeOut(element, duration / 3)
-            }, duration / 3);
-        },
-
-
+            }
     }
+}
 
-    function getTransform(translation, ratio) {
-        const result = [];
-        if (translation) {
-            result.push(`translate(${translation.x}px,${translation.y}px)`);
-        }
-        if (ratio) {
-            result.push(`scale(${ratio})`);
-        }
-        return result.join(' ');
+
+function getTransform(translation, ratio) {
+    const result = [];
+    if (translation) {
+        result.push(`translate(${translation.x}px,${translation.y}px)`);
     }
+    if (ratio) {
+        result.push(`scale(${ratio})`);
+    }
+    return result.join(' ');
 }
