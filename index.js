6d55c205742e4e9d08ea5e9b09a41aad5e1c6e4a
senyapekus <ksenya.pshenichnikova03@gmail.com>
2023-03-13 14:23:37 +0500

    4-8

--- index.js
+++ index.js
@@ -1,30 +1,62 @@
 addListeners();
 
 function addListeners() {
-    document.getElementById("fadeInPlay")
-        .addEventListener("click", function () {
-            const block = document.getElementById("fadeInBlock");
+    document.getElementById('fadeInPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('fadeInBlock');
             animaster().fadeIn(block, 5000);
         });
 
-    document.getElementById("fadeOutPlay")
-        .addEventListener("click", function () {
-            const block = document.getElementById("fadeOutBlock");
+    document.getElementById('fadeOutPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('fadeOutBlock');
             animaster().fadeOut(block, 5000);
         });
 
-    document.getElementById("movePlay")
-        .addEventListener("click", function () {
-            const block = document.getElementById("moveBlock");
+    document.getElementById('moveAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveAndHideBlock');
+            block.unchangedClassList = block.classList;
+            block.moveAndHide = animaster().moveAndHide(block, 5000);
+        });
+
+    document.getElementById('moveAndHideReset')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveAndHideBlock');
+            block.moveAndHide.stop()
+        });
+
+    document.getElementById('showAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('showAndHideBlock');
+            animaster().showAndHide(block, 6000);
+        });
+
+    document.getElementById('heartBeatingPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('heartBeatingBlock');
+            block.heartBeating = animaster().heartBeating(block);
+        });
+
+    document.getElementById('heartBeatingStop')
+        .addEventListener('click', function () {
+            const block = document.getElementById('heartBeatingBlock');
+            block.heartBeating.stop();
+        });
+
+    document.getElementById('scalePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('scaleBlock');
+            animaster().scale(block, 1000, 1.4);
+        });
+
+    document.getElementById('movePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveBlock');
             animaster().move(block, 1000, {x: 100, y: 10});
 
         });
 
-    document.getElementById("scalePlay")
-        .addEventListener("click", function () {
-            const block = document.getElementById("scaleBlock");
-            animaster().scale(block, 1000, 1.25);
-        });
 }
 
 function getTransform(translation, ratio) {
@@ -43,29 +75,132 @@ function animaster() {
     return new Animaster();
 }
 
+class Step {
+    constructor(name,duration,params) {
+        this.name = name;
+        this.duration = duration;
+        this.params = params;
+    }
+}
+
 class Animaster {
 
     _steps = [];
 
-    fadeIn(element, duration) {
-        element.style.transitionDuration = `${duration}ms`;
-        element.classList.remove("hide");
-        element.classList.add("show");
+    addStep(name,duration,params){
+        this._steps.push(new Step(name,duration,params));
+        return this;
     }
 
-    fadeOut(element, duration) {
-        element.style.transitionDuration = `${duration}ms`;
-        element.classList.add("hide");
-        element.classList.remove("show");
+    _createFuncArray(){
+        let _funcs = []
+        _funcs['move'] = this.move;
+        _funcs['scale'] = this.scale;
+        _funcs['fadeIn'] = this.fadeIn;
+        _funcs['fadeOut'] = this.fadeOut;
+        _funcs['reset'] = this.reset;
+        _funcs['moveAndHide'] = this.moveAndHide;
+        _funcs['showAndHide'] = this.showAndHide;
+        _funcs['heartBeating'] = this.heartBeating;
+        _funcs['addDelay'] = this.addDelay;
+
+        return _funcs;
     }
 
-    scale(element, duration, ratio) {
-        element.style.transitionDuration = `${duration}ms`;
+    play(element,cycled=false){
+
+        const unchangedClassList = element.classList;
+        let stopRequested = false;
+        let stepInd = 0;
+
+        const funcs = this._createFuncArray();
+
+        function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+        const playNextStep = () => {
+            if(stopRequested)
+                return;
+
+            let step = this._steps[stepInd++];
+            if(cycled && stepInd >= this._steps.length)
+                stepInd = 0;
+            if (step === undefined)
+                return;
+
+            funcs[step.name](element,step.duration,step.params);
+
+            sleep(step.duration).then(()=>{
+                playNextStep();
+            });
+        }
+
+        playNextStep();
+
+        const stop = () => {
+            this.reset(element, unchangedClassList);
+            stopRequested = true;
+        }
+
+        return {
+            stop
+        }
+    }
+
+    scale(element,duration,ratio) {
+        element.style.transitionDuration =  `${duration}ms`;
         element.style.transform = getTransform(null, ratio);
     }
 
     move(element, duration, translation) {
         element.style.transitionDuration = `${duration}ms`;
         element.style.transform = getTransform(translation, null);
+    }
+
+    fadeIn(element, duration) {
+        element.style.transitionDuration =  `${duration}ms`;
+        element.classList.remove('hide');
+        element.classList.add('show');
+    }
+
+    fadeOut(element, duration){
+        element.style.transitionDuration =  `${duration}ms`;
+        element.classList.add('hide');
+        element.classList.remove('show');
+    }
+
+    reset(element, classList = null) {
+        element.style.transitionDuration = null;
+        element.style.transform = null;
+        element.classList = classList === null ? null : classList.value;
+        if(classList.value.includes('hide'))
+            classList.add('show');
+        if(classList.value.includes('show'))
+            classList.add('hide');
+    }
+
+    addDelay(duration){
+        return new Promise(resolve => setTimeout(resolve, duration)).then();
+    }
+
+    moveAndHide(element, duration){
+        this.addStep('move',duration * 0.4, {x: 100, y: 20});
+        this.addStep('fadeOut',duration*0.6);
+        return this.play(element);
+    }
+
+    showAndHide(element, duration){
+        duration = duration*0.33;
+        this.addStep('fadeIn',duration);
+        this.addStep('addDelay', duration);
+        this.addStep('fadeOut',duration);
+        this.play(element);
+    }
+
+    heartBeating(element){
+        this.addStep('scale',500, 1.4)
+        this.addStep('scale',500, 1)
+        return this.play(element,true);
     }
 }
