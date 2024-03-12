31fe129794642dcdf5c48fe1ae2cc1e85673f2f0
NeonyFenyx <idrishal277@gmail.com>
2024-03-12 20:03:14 +0500

    add reset

--- index.js
+++ index.js
@@ -1,88 +1,88 @@
 addListeners();
 
 function addListeners() {
+    let resetObj;
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            animaster().fadeIn(block, 5000);
+            resetObj = animaster().fadeIn(block, 5000);
         });
 
     document.getElementById('fadeInReset')
         .addEventListener('click', function () {
-            animaster().reset();
+            resetObj.reset();
         });
 
     document.getElementById('moveAndHidePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveAndHideBlock');
-            animaster().moveAndHide(block, 5000);
+            resetObj = animaster().moveAndHide(block, 5000);
         });
 
     document.getElementById('moveAndHideReset')
         .addEventListener('click', function () {
-            animaster().reset();
+            resetObj.reset();
         });
 
     document.getElementById('showAndHidePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('showAndHideBlock');
-            animaster().showAndHide(block, 5000);
+            resetObj = animaster().showAndHide(block, 5000);
         });
 
     document.getElementById('showAndHideReset')
         .addEventListener('click', function () {
-            animaster().reset();
+            resetObj.reset();
         });
 
-    let stopObj;
     document.getElementById('heartBeatingPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('heartBeatingBlock');
-            stopObj = animaster().heartBeating(block);
+            resetObj = animaster().heartBeating(block);
         });
 
     document.getElementById('heartBeatingStop')
         .addEventListener('click', function () {
-            stopObj.stop();
+            resetObj.stop();
         });
 
     document.getElementById('heartBeatingReset')
         .addEventListener('click', function () {
-            animaster().reset();
+            resetObj.reset();
         });
 
 
     document.getElementById('fadeOutPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeOutBlock');
-            animaster().fadeOut(block, 5000);
+            resetObj = animaster().fadeOut(block, 5000);
         });
 
     document.getElementById('fadeOutReset')
         .addEventListener('click', function () {
-            animaster().reset();
+            resetObj.reset();
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            animaster().move(block, 1000, {x: 100, y: 10});
+            resetObj = animaster().move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('moveReset')
         .addEventListener('click', function () {
-            animaster().reset();
+            resetObj.reset();
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            animaster().scale(block, 1000, 1.25);
+            resetObj = animaster().scale(block, 1000, 1.25);
         });
 
     document.getElementById('scaleReset')
         .addEventListener('click', function () {
-            animaster().reset();
+            resetObj.reset();
         });
 }
 
@@ -119,37 +119,58 @@ function animaster() {
     const _steps = []
     let _currentStep = 0;
     let _currentTimeout = null;
+    let _stop = () => void 0;
 
     const play = function (element) {
-        if(_currentStep === 0) {
+        if(_currentStep >= _steps.length) {
             return;
         }
 
         const step = _steps[_currentStep]
         step.action(element, step.duration, step.arg)
+        _currentStep++;
         _currentTimeout = setTimeout(() => play(element), step.duration);
-    }
 
-    const reset = () => clearTimeout(_currentTimeout);
+
+        return {
+            _element: element,
+            _transform: element.transform,
+            _isShown: element.classList.contains('show'),
+            stop: _stop,
+            reset : function () {
+                clearTimeout(_currentTimeout);
+                this._element.style.transitionDuration = null;
+                element.transform = this._transform;
+                this._element.style.transform = null;
+                if(this._isShown) {
+                    this._element.classList.add('show');
+                    this._element.classList.remove('hide');
+                } else {
+                    this._element.classList.add('hide');
+                    this._element.classList.remove('show');
+                }
+            }
+        }
+    }
 
     const fadeIn = function (element, duration) {
         addFadeIn(duration);
-        play(element);
+        return play(element);
     };
 
     const fadeOut = function(element, duration) {
         addFadeOut(duration);
-        play(element);
+        return play(element);
     };
 
     const move = function (element, duration, translation) {
         addMove(duration, translation);
-        play(element);
+        return play(element);
     };
 
     const scale = function (element, duration, ratio) {
         addScale(duration, ratio);
-        play(element);
+        return play(element);
     };
 
     const addAction = function (action, duration, arg) {
@@ -158,7 +179,6 @@ function animaster() {
             duration,
             arg
         });
-        _currentStep++;
     };
 
     const addWait = (duration) => addAction(() => void 0, duration)
@@ -210,16 +230,21 @@ function animaster() {
     };
 
     const heartBeating = function (element) {
+        let isStopped = false;
+
         let beat = function () {
             const ratio = 1.4;
             const segmentTime = 500;
             addScale(segmentTime, ratio);
             addScale(segmentTime, 1 / ratio);
-            addAction(beat, 0);
+            if(!isStopped) {
+                addAction(beat, 0);
+            }
         };
 
         addAction(beat);
-        play(element);
+        _stop = () => isStopped = true;
+        return play(element);
     };
 
     return {
