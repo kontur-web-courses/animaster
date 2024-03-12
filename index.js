489605fd18d8ec5eebcd12cf3eef14b1a85ccfe0
Batuev Nikita <batueff.nikita@yandex.ru>
2024-03-12 18:00:28 +0500

    peb

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
 addListeners();
 
 function animaster(){
-    animaster.fadeIn(element, duration) {
+    this.fadeIn = function (element, duration) {
         element.style.transitionDuration =  `${duration}ms`;
         element.classList.remove('hide');
         element.classList.add('show');
     }
-    animaster.fadeOut(element, duration) {
+    this.fadeOut = function (element, duration) {
         element.style.transitionDuration =  `${duration}ms`;
         element.classList.remove('show');
         element.classList.add('hide');
     }
-    animaster.move(element, duration, translation) {
+    this.move = function (element, duration, translation) {
         element.style.transitionDuration = `${duration}ms`;
         element.style.transform = getTransform(translation, null);
     }
-    animaster.scale(element, duration, ratio) {
+    this.scale = function (element, duration, ratio) {
         element.style.transitionDuration =  `${duration}ms`;
         element.style.transform = getTransform(null, ratio);
     }
+
+    return this;
 }
 function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            animaster().fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
+            animaster().move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            animaster().scale(block, 1000, 1.25);
         });
 }
 
