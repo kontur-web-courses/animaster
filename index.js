3c869bad59f8248b81aadcbd5d4f24f249183fb7
LegendaV <115637903+LegendaV@users.noreply.github.com>
2024-03-12 18:00:49 +0500

    FadeOut

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ function animaster() {
             element.classList.remove('hide');
             element.classList.add('show');
         },
+
+        fadeOut(element, duration) {
+            element.style.transitionDuration = `${duration}ms`;
+            element.classList.remove('show');
+            element.classList.add('hide');
+        },
         /**
          * Функция, передвигающая элемент
          * @param element — HTMLElement, который надо анимировать
@@ -40,6 +46,12 @@ function addListeners() {
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
             animaster().fadeIn(block, 5000);
+        });
+
+    document.getElementById('fadeOutPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('fadeOutBlock');
+            animaster().fadeOut(block, 5000);
         });
 
     document.getElementById('movePlay')
