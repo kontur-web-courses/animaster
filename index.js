7c68ffb144065ba9d31b8d45c4332935976814d2
yellooot <njkzrhjkz@gmail.com>
2024-03-12 18:50:43 +0500

    Add 5

--- index.js
+++ index.js
@@ -19,11 +19,18 @@ function addListeners() {
             animaster().showAndHide(block, 5000);
         });
 
+    let stopObj;
     document.getElementById('heartBeatingPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('heartBeatingBlock');
-            animaster().heartBeating(block);
+            stopObj = animaster().heartBeating(block);
         });
+
+    document.getElementById('heartBeatingStop')
+        .addEventListener('click', function () {
+            stopObj.stop();
+        });
+
 
     document.getElementById('fadeOutPlay')
         .addEventListener('click', function () {
@@ -131,17 +138,25 @@ function animaster() {
         }, segmentTime * 2);
     }
 
-    const heartBeating = function (element, duration) {
+    const heartBeating = function (element) {
         const beat = function() {
             const segmentTime = 500;
             scale(element, segmentTime, 1.4);
             setTimeout(() => {
                 scale(element, segmentTime, 5 / 7);
-            }, segmentTime);
-        }
+            }, segmentTime)
+        };
 
-        setInterval(() => beat(), 1000);
-    }
+        const intervalId = setInterval(() => beat(), 1000);
+
+        const stop = function () {
+            clearInterval(intervalId);
+        };
+
+        return {
+            stop
+        };
+    };
 
     return {
         fadeIn,
