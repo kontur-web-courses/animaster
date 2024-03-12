bf59dcb0711636fad255817d0c5833ff1d3b0a7d
magnus5552 <isackov.stepa@yandex.ru>
2024-03-12 18:08:42 +0500

    add stop and reset

--- index.js
+++ index.js
@@ -1,37 +1,29 @@
 addListeners();
 
 function addListeners() {
-
   addPlayListener("fadeIn", (anim, block) => {
-    anim.addFadeIn(1000)
-        .play(block);
+    return anim.addFadeIn(1000).play(block);
   });
 
   addPlayListener("move", (anim, block) => {
-    anim.addMove(1000, { x: 100, y: 10 })
-        .play(block);
+    return anim.addMove(1000, { x: 100, y: 10 }).play(block);
   });
 
   addPlayListener("scale", (anim, block) => {
-    anim.addScale(1000, 1.25)
-        .play(block);
+    return anim.addScale(1000, 1.25).play(block);
   });
 
   addPlayStopListeners("moveAndHide", (anim, block) => {
-    anim.moveAndHide(5000)
-        .play(block);
+    return anim.moveAndHide(5000).play(block);
   });
 
   addPlayListener("showAndHide", (anim, block) => {
-    anim.showAndHide(1000)
-        .play(block);
+    return anim.showAndHide(1000).play(block);
   });
 
   addPlayStopListeners("heartBeating", (anim, block) => {
-      anim.heartBeating(block)
-          .play(block, cycled=true)
-    }
-  );
+    return anim.heartBeating(block).play(block, (cycled = true));
+  });
 }
 
 function addPlayStopListeners(name, callback) {
@@ -41,9 +33,7 @@ function addPlayStopListeners(name, callback) {
   });
 
   addClickListener(name + "Stop", name + "Block", (_, __) => {
-    for (const stop of stops) {
-      stop.stop();
-    }
+    stops.forEach(e => e.stop());
   });
 }
 
@@ -62,44 +52,63 @@ function addClickListener(playName, blockName, callback) {
 function animaster() {
   return {
     _steps: [],
+    _timeouts: [],
 
     play(element, cycled = false) {
-      const interval = setInterval(
-        () => {
-          let offset = 0;
-          for (const { func, duration, args } of this._steps) {
-            setTimeout(func, offset, element, duration, ...args);
-            offset += duration;
-          }
-          if (!cycled) {
-            clearInterval(interval);
-          }
+      this._play(element);
+      if (cycled) {
+        var interval = setInterval(
+          this._play,
+          this._steps.reduce((s, e) => s + e.duration, 0)
+        );
+      }
+
+      return {
+        stop: () => clearInterval(interval),
+        reset: () => {
+          this._steps.forEach((e) => e.cancel());
+          this._timeouts.forEach((e) => clearTimeout(e));
         },
-        this._steps.reduce((s, e) => s + e.duration, 0));
+      };
+    },
+
+    _play(element) {
+      let offset = 0;
+      this._steps.forEach(({ func, duration, args }) => {
+        this._timeouts.push(
+          setTimeout(func, offset, element, duration, ...args)
+        );
+        offset += duration;
+      });
     },
 
     addMove(duration, ...args) {
-      return this.addStep(this.move, duration, args);
+      return this.addStep(this.move, resetMoveAndScale, duration, args);
     },
 
     addScale(duration, ...args) {
-      return this.addStep(this.scale, duration, args);
+      return this.addStep(this.scale, resetMoveAndScale, duration, args);
     },
 
     addFadeIn(duration, ...args) {
-      return this.addStep(this.fadeIn, duration, args);
+      return this.addStep(this.fadeIn, resetFadeIn, duration, args);
     },
 
     addFadeOut(duration, ...args) {
-      return this.addStep(this.fadeOut, duration, args);
+      return this.addStep(this.fadeOut, resetFadeOut, duration, args);
     },
 
-    addDelay(duration) {
-      return this.addStep(() => {}, duration);
+    addDelay(duration, ...args) {
+      return this.addStep(
+        () => {},
+        () => {},
+        duration,
+        args
+      );
     },
 
-    addStep(func, duration, args) {
-      this._steps.push({ func, duration, args });
+    addStep(func, cancel, duration, args) {
+      this._steps.push({ func, cancel, duration, args });
       return this;
     },
 
@@ -137,35 +146,16 @@ function animaster() {
     showAndHide(duration) {
       const fadeDuration = duration / 3;
       this.addFadeIn(fadeDuration)
-          .addDelay(fadeDuration)
-          .addFadeOut(fadeDuration);
+        .addDelay(fadeDuration)
+        .addFadeOut(fadeDuration);
       return this;
     },
 
     heartBeating(element) {
-      const timeouts = [null, null];
-
-      const interval = setInterval(() => {
-        timeouts[0] = setTimeout(this.scale, 0, element, 500, 1.4);
-        timeouts[1] = setTimeout(this.scale, 500, element, 500, 1);
-      }, 1000);
-      return {
-        reset: resetMoveAndScale,
-        interval,
-        timeouts,
-        element,
-        stop,
-      };
+      this.addScale(500, 1.4).addScale(500, 1);
+      return this;
     },
   };
-
-  function stop() {
-    for (const timeout of this.timeouts) {
-      clearTimeout(timeout);
-    }
-    clearInterval(this.interval);
-    this.reset(this.element);
-  }
 
   function resetMoveAndScale(element) {
     element.style.transitionDuration = null;
